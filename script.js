6bf7448bd461a1857b4fc524c0d01f56139300cc
sernadesigns <sernadesigns@gmail.com>
2021-04-25 12:01:28 -0600

    adds tweet function and wires up buttons

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 let apiQuotes = [];
 
 // Show New Quote
 function newQuote() {
 	// Pick a random quote from apiQuotes array
 	const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-	console.log(quote);
+	// Check if author field is blank and replace it with 'Unknown'
+	authorText.textContent = quote.author ? quote.author : 'Unknown';
+	// Check quote length to determine the styling
+	if (quote.text.length > 120) {
+		quoteText.classList.add('long-quote');
+	} else {
+		quoteText.classList.remove('long-quote');
+	}
+	quoteText.textContent = quote.text;
 }
 
 // Get Quotes From API
@@ -18,6 +32,16 @@ async function getQuotes() {
 		// Catch Error Here
 	}
 }
+
+// Tweet Quote
+function tweetQuote() {
+	const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+	window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 // On Load
 getQuotes();
